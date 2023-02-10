01ca41aecb88888644bf039ee70146a8e8ca8050
Dragos Moraru <moraru.etc@gmail.com>
2023-02-10 13:50:08 +0200

    added gcp logging artifact

--- src/main/java/com/carrefour/giftc/controllers/GiftCardController.java
+++ src/main/java/com/carrefour/giftc/controllers/GiftCardController.java
@@ -1,15 +1,20 @@
 package com.carrefour.giftc.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class GiftCardController {
+    Logger logger = LoggerFactory.getLogger(GiftCardController.class);
     @Value("${app.message}")
     private String message;
     @GetMapping("/hello")
     public String hello(){
+
+        logger.info("POS request from {}", 1234);
         return message;
     }
 }
